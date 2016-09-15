e6445c96b5a777afc339fb55ff80e6dee634d842
Luong Quoc Viet <luongviet82@gmail.com>
2016-09-15 20:44:04 +0700

    content
    content

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,40 +7,73 @@
 	</head>
 	<body>
 		
-		<div class="col-lg-12 text-center">
-	       	<h1 class="text-center">Get early access</h1>
-	        <div class="col-lg-12">
-	            <div class="col-lg-offset-2 col-lg-8 text-center">
-	                <form class="form-inline">
-	                    <div class="form-group col-lg-12">
-	                    	<div class="col-lg-4">
-	                        	<input type="text" name="youname" class="form-control" placeholder="Youname">
-                        	</div>
-                        	<div class="col-lg-4">
-	                        	<input type="email" class="form-control" name="email" placeholder="E-mail">
-                        	</div>                           
-	                    </div>
-	                    <div class="form-group col-lg-12">
-	                    	<div class="col-lg-4">
+		<div id="header-home">
+			<div class="container">
+				<div class="top">
+					<div class="pull-left">
+						<a href="#">Trips</a>
+						<a href="#">Blog</a>
+					</div>
+				
+					<div class="pull-right">
+						<a href="#">Sign In</a>
+						<a href="#"><i class="fa fa-shopping-cart"></i>Cart</a>
+					</div>
+					<div class="text-center">
+						<a href="#"><img src=""></a>
+						trip<span>shoes</span>
+					</div>
+				</div>
+
+				<div id="banner-header">
+					<h5>Daily Guided Trips in Hoi An</h5>
+					<h1>Take a trip in someone else's shoes</h1>
+					<a href="#" class="btn btn-lg"></a>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="content">
+			<div class="col-lg-12 text-center">
+		       	<h1 class="text-center">Get early access</h1>
+		        <div class="col-lg-12">
+		            <div class="col-lg-offset-2 col-lg-8 text-center">
+		                <form class="form-inline">
+		                    <div class="form-group col-lg-12">
+		                    	<div class="col-lg-4">
+		                        	<input type="text" name="youname" class="form-control" placeholder="Youname">
+	                        	</div>
+	                        	<div class="col-lg-4">
+		                        	<input type="email" class="form-control" name="email" placeholder="E-mail">
+	                        	</div>                           
+		                    </div>
+		                    <div class="form-group col-lg-12">
+		                    	<div class="col-lg-4">
+			                        <select class="form-control">
+			                            <option>When will you being Hoi An?	</option>
+			                        </select>
+		                        </div>
+		                        <div class="col-lg-4">
+		                        	<input type="text" name="country" class="form-control" placeholder="Which country are you from?">
+	                        	</div>
+		                    </div>
+		                    <div class="col-lg-12">
 		                        <select class="form-control">
-		                            <option>When will you being Hoi An?	</option>
+		                            <option>Which trip are you most intered in?</option>
 		                        </select>
-	                        </div>
-	                        <div class="col-lg-4">
-	                        	<input type="text" name="country" class="form-control" placeholder="Which country are you from?">
-                        	</div>
-	                    </div>
-	                    <div class="col-lg-12">
-	                        <select class="form-control">
-	                            <option>Which trip are you most intered in?</option>
-	                        </select>
-	                    </div>
-	                    <button class="btn btn-lg btn-success col-lg-12">Get me early access</button>
-	                </form>
-	            </div>
-	        </div>
+		                    </div>
+		                    <button class="btn btn-lg btn-success col-lg-12">Get me early access</button>
+		                </form>
+		            </div>
+		        </div>
+		    </div>
 	    </div>
-		
+
+	    <div id="footer">
+	    	
+	    </div>
+			
 		<script type="text/javascript" src="{{ asset('/jquery/jquery.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('/bootstrap/js/bootstrap.min.js') }}"></script>
 	</body>
