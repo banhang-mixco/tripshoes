fdfc434ebc068a4d1d749421dc2a6e11ce792870
Nguyen Thanh Phuoc <nguyenthanhphuoc0503@gmail.com>
2016-09-16 21:25:35 +0700

    page 16 and 18

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('frontend.index');
 });
+Route::get('/tripsnologin', function(){
+	return view('frontend.trips_no_login');
+});
+Route::get('/aboutus', function(){
+	return view('frontend.aboutUs');
+});
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -140,17 +140,17 @@
 				<h1 class="text-center"> Booking is as easy as 1,2,3</h1>
 				<div class="col-lg-12">
 					<div class="col-lg-4 text-center">
-						<img src="{{ asset('/images/noun_464.png') }}">
+						<img src="{{ asset('frontend/images/noun_464.png') }}">
 						<h3 class="text-center"> Download the TripShoes app</h3>
 						<p class="text-center"> TripShoes is currently only available on iphone. Simply download the secure TripShoes app onto your iphone on the App Store</p>
 					</div>
 					<div class="col-lg-4 text-center">
-						<img src="{{ asset('/images/noun_579236.png') }}">
+						<img src="{{ asset('frontend/images/noun_579236.png') }}">
 						<h3 class="text-center"> Book a trip in-app or on the website</h3>
 						<p class="text-center"> Payments can be made via Paypal or credit card. All trips offer incredible value.</p>
 					</div>
 					<div class="col-lg-4 text-center">
-						<img src="{{ asset('/images/noun_532311.png') }}">
+						<img src="{{ asset('frontend/images/noun_532311.png') }}">
 						<h3 class="text-center"> Meet at the Starting Location</h3>
 						<p class="text-center"> Arrive ad your starting locaition 5 minutes before your trip begins, log into your app, tap on Tour Mode and you're all ser. TripShoes will handle the rest.</p>
 					</div>
@@ -166,26 +166,27 @@
 					<p class="text-center">TripShoes is soft launching in Hoi An in October 2016. If you're in Hoi An in late 2016, registe now for ealy access and be amongst the first to experience one of our unique trips. Trips in this period will be heavily discounted so register quickly for early access trips.</p>
 				</div>
 			</div>
+			<!-- form -->
 			<div class="col-lg-12 text-center">
 		       	<h1 class="text-center">Get early access</h1>
 	            <div class="col-lg-offset-2 col-lg-8 text-center">
 	                <form class="form-inline">
 	                    <div class="col-lg-12">
-	                    	<div class="col-lg-6">
-	                        	<input type="text" name="youname" class="form-control" placeholder="Youname">
+                			<div class="col-lg-6">
+	                        	<input type="text" name="youname" class="form-control" placeholder="Youname" width="100%">
                         	</div>
-                        	<div class="col-lg-6">
-	                        	<input type="email" class="form-control" name="email" placeholder="E-mail">
+                			<div class="col-lg-6">
+	                        	<input type="email" class="form-control" name="email" placeholder="E-mail" width="100%">
                         	</div>                           
 	                    </div>
 	                    <div class="col-lg-12">
-	                    	<div class="col-lg-12">
+	                    	<div class="col-lg-6">
 		                        <select class="form-control">
 		                            <option>When will you being Hoi An?	</option>
 		                        </select>
 	                        </div>
-	                        <div class="col-lg-12">
-	                        	<input type="text" name="country" class="form-control" placeholder="Which country are you from?">
+	                        <div class="col-lg-6">
+	                        	<input type="text" name="country" class="form-control" placeholder="Which country are you from?" width="100%" >
                         	</div>
 	                    </div>
 	                    <div class="col-lg-12">
@@ -197,10 +198,7 @@
 	                </form>
 	            </div>
 		    </div>
-		</div>
-
-			
-	    
+		<!-- end form -->	    
 
 	    <div id="footer">
 	    	<div class="container">
