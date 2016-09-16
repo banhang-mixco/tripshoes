3605fb38b38dcf4023399013e13f505de3919bfb
Luong Quoc Viet <luongviet82@gmail.com>
2016-09-16 08:26:13 +0700

    change3
    change3

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -84,17 +84,57 @@
 							<img src="{{ asset('/images/smartmockups-129.png') }}" width="100%" height="100%">
 						</div>
 					</div>
-
+					
 					<h1>A Photobook of Memories</h1>
 					<p>During your trip you'll be able to take photos with a single tap and send messages instantly to the rest of your group without having to leave the app at anytime. With the ability to upload photos, group chat and send instant messages to your fellow travellers in Photobook - every image, comment, or emoji is now saved forever.</p>
 				
 				
 					<img src="{{ asset('/images/Image21.png') }}" width="100%" height="100%">
 						
-					</div>
 				</div>
 			</div>
 
+			<!-- Begin A new form of giving -->
+			<div class="col-lg-12 ">
+				<div class="col-lg-offset-1 col-lg-5">
+					<h1 class="text-center"> A new form of giving</h1>
+					<p class="text-center"> Every booking undertaken on TripShoes sees a donation made to assist the under privileged living in Vietnam. TripShoes is associated with a host of charities and education centres across Vietnam an Australia and you'll be pleased to know you'll be "giving with every foottep indertaken on each trip."</p>
+				</div>
+			</div>
+			<!-- End giving -->
+
+			<!-- How to book -->
+			<div class="col-lg-12">
+				<h4 class="text-center">How to Book</h4>
+				<h1 class="text-center"> Booking is as easy as 1,2,3</h1>
+				<div class="col-lg-12">
+					<div class="col-lg-4 text-center">
+						<img src="{{ asset('/images/noun_464.png') }}">
+						<h3 class="text-center"> Download the TripShoes app</h3>
+						<p class="text-center"> TripShoes is currently only available on iphone. Simply download the secure TripShoes app onto your iphone on the App Store</p>
+					</div>
+					<div class="col-lg-4 text-center">
+						<img src="{{ asset('/images/noun_579236.png') }}">
+						<h3 class="text-center"> Book a trip in-app or on the website</h3>
+						<p class="text-center"> Payments can be made via Paypal or credit card. All trips offer incredible value.</p>
+					</div>
+					<div class="col-lg-4 text-center">
+						<img src="{{ asset('/images/noun_532311.png') }}">
+						<h3 class="text-center"> Meet at the Starting Location</h3>
+						<p class="text-center"> Arrive ad your starting locaition 5 minutes before your trip begins, log into your app, tap on Tour Mode and you're all ser. TripShoes will handle the rest.</p>
+					</div>
+				</div>
+			</div>
+			<!-- End how to book -->
+
+			<!-- Begin Launch -->
+			<div class="col-lg-12">
+				<div class="col-lg-offset-1 col-lg-10">
+					<h3 class="text-center"> Launch</h3>
+					<h1 class="text-center"> Soft launch in Hoi An October 2016</h1>
+					<p class="text-center">TripShoes is soft launching in Hoi An in October 2016. If you're in Hoi An in late 2016, registe now for ealy access and be amongst the first to experience one of our unique trips. Trips in this period will be heavily discounted so register quickly for early access trips.</p>
+				</div>
+			</div>
 			<div class="col-lg-12 text-center">
 		       	<h1 class="text-center">Get early access</h1>
 		        <div class="col-lg-12">
@@ -128,7 +168,10 @@
 		            </div>
 		        </div>
 		    </div>
-	    </div>
+		</div>
+
+			
+	    
 
 	    <div id="footer">
 	    	<div class="container">
