f65f800bfe7d40c772d5bb58a43e8d9cc51536c6
Luong Quoc Viet <luongviet82@gmail.com>
2016-09-22 13:44:58 +0700

    dsa
    đasa

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -11,7 +11,7 @@
 			<div class="container">
 				<div class="top">
 					<div class="pull-left">
-						<a href="#">Trips</a>
+						<a href="#">Trips</a
 						<a href="#">Blog</a>
 					</div>
 				
