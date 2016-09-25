0fc588bd748099e1dbd8ae5266f348bf205c584b
Nguyen Thanh Phuoc <nguyenthanhphuoc0503@gmail.com>
2016-09-25 23:48:12 +0700

    fix css

---
+++ resources/views/frontend/send_us_email.blade.php
@@ -0,0 +1,44 @@
+@extends('frontend.layout.master')
+	
+@section('content')
+<div class="container">
+<div class="row">
+	<div class="col-lg-12">
+        <h1 class="text-center">Send us a message</h1>
+        <div class="col-lg-offset-3 col-lg-6 text-center">
+        	<h3>Fell free to send us a an email with any question you might have</h3>
+    	</div>
+        <div class="col-lg-12">
+            <div class="col-lg-12 text-center form_send">
+	        <div class="col-lg-offset-2 col-lg-8 text-center">
+	            <form class="form_access">
+                 <div class="row">
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                        <input type="text" name="youname" class="form-control form_padding input-lg" placeholder="Youname" width="100%">
+                    </div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="form-group">
+                        <input type="email" class="form-control form_padding input-lg" name="email" placeholder="E-mail" width="100%">
+                    </div>
+                   </div>
+                  </div>
+                  <div class="row">
+                   <div class="col-lg-12">
+                    <div class="form-group">
+                        <textarea cols="70" rows="5" resize="false" class="form-control form_padding" name="message" placeholder="Message"></textarea>
+                    </div>
+                   </div>
+                  </div>
+                  <button class="btn btn-lg btn_login col-lg-12 form_padding">Get in touch</button>
+                </form>
+                <div class="text-center col-lg-12 just_email"> Or just email on</div>
+                <div class="text-center col-lg-12 info"><u>info@tripshoes.com</u></div>
+	        </div>
+	    </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
