575ee8181022589dc9fe03aa7e4db277558093d1
Luong Quoc Viet <luongviet82@gmail.com>
2016-09-26 13:28:52 +0700

    support
    support

--- resources/views/frontend/support.blade.php
+++ resources/views/frontend/support.blade.php
@@ -1,25 +1,65 @@
 @extends('frontend.layout.master')
 
 @section('content')
-	<div class="container">
+	<div class="support">
 		<h1 class="text-center">Frequently Asked Question</h1>
-		<div class="col-lg-offset-2 col-lg-8">
-			<span> WILL THERE BE A LIVE GUIDE WITH ME AT ALL TIME</span>
-			<ul><li>+</li></ul>
-		</div>
-		<div class="col-lg-offset-2 col-lg-8">
-			<span>WHAT IF I RUN OUT OF BATTERIES ON MY PHONE MID-TRIP?</span>
-			<ul><li>+</li></ul>
-		</div>
-		<div class="col-lg-offset-2 col-lg-8">
-			<span> HOW LONG DO MOST TRIPS LAST FOR?</span>
-			<ul>
-				<li>Return are subject to the merchant's return policy. Please contact the merchant if you would like to return you item(s). Affirm will only issue you a refund if the merchant receives your returned items and process refund within 120 days from the day of purchase. We will credir any payments you have made on your loan, up to the amount of the refund, to the original form of payment within 3 to 10 business days of the refund. If, however, the merchant issues you store credit, you will still be responsible for repaying your loan.</li>
-			</ul>
-		</div>
-		<div class="col-lg-offset-2 col-lg-8">
-			<span> CAN I MAKE A BOOKING ON THE DAY OF TRAVEL?</span>
-			<ul><li>+</li></ul>
-		</div>
-	</div>
+		<ul class="support-step">
+			<li>
+				<div class="row">
+					<div class="col-lg-1">
+						<span class="border-number">1</span>
+					</div>
+					<div class="col-lg-10">
+						<h4>WILL THERE BE A LIVE GUIDE WITH ME AT ALL TIME</h4>
+					</div>
+					<div class="col-lg-1">
+						<span class="pull-right sign">+</span>
+					</div>
+				</div>
+				
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-lg-1">
+						<span class="border-number">2</span>
+					</div>
+					<div class="col-lg-10">
+						<h4>HOW LONG DO MOST TRIPS LAST FOR?</h4>
+					</div>
+					<div class="col-lg-1">
+						<span class="pull-right sign">+</span>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-lg-1">
+						<span class="border-number">3</span>
+					</div>
+					<div class="col-lg-10">
+						<h4>WHAT IF I RUN OUT OF BATTERIES ON MY PHONE MID-TRIP?</h4>
+						<p>Return are subject to the merchant's return policy. Please contact the merchant if you would like to return you item(s). Affirm will only issue you a refund if the merchant receives your returned items and process refund within 120 days from the day of purchase. We will credir any payments you have made on your loan, up to the amount of the refund, to the original form of payment within 3 to 10 business days of the refund. If, however, the merchant issues you store credit, you will still be responsible for repaying your loan.</p>
+					</div>
+					<div class="col-lg-1">
+						<span class="pull-right sign">+</span>
+					</div>
+				</div>
+				
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-lg-1">
+						<span class="border-number">4</span>
+					</div>
+					<div class="col-lg-10">
+						<h4>CAN I MAKE A BOOKING ON THE DAY OF TRAVEL?</h4>
+					</div>
+					<div class="col-lg-1">
+						<span class="pull-right sign">+</span>
+					</div>
+				</div>
+			</li>
+		</ul>
+		
+		
 @endsection
